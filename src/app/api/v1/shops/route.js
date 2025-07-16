0c4b0595d41d363bb24b3536b75539913f8379c4
rimaakter1 <rimac201262@gmail.com>
2025-07-16 16:44:48 +0600

    shop creation limit check for free trial

--- src/app/api/v1/shops/route.js
+++ src/app/api/v1/shops/route.js
@@ -43,7 +43,7 @@ export async function POST(request) {
     return NextResponse.json({ error: "...not authorized" }, { status: 404 });
 
   // Check shop limit before allowing to create
-  const currentShopUsage = user.usage?.shop;
+  const currentShopUsage = user.usage?.shops;
   const allowedShopLimit = user.subscriptionScope?.shops;
 
   console.log(currentShopUsage, allowedShopLimit)
