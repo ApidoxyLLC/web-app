067f6d3432ccc62b93c255f83526c96d2fa2f3e6
MamunApidoxy <mamun@apidoxy.com>
2025-07-24 17:43:36 +0600

    ...

--- src/models/vendor/Vendor.js
+++ src/models/vendor/Vendor.js
@@ -183,7 +183,7 @@ const marketingSchema = new mongoose.Schema({
 
 const stuffSchema = new mongoose.Schema(
   {      userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false },
-    designation: { type: String, enum: [ 'store_manager', 'assistant_manager', 'cashier', 'sales_associate', 'inventory_clerk', 'security', 'janitor', 'other' ], required: fasle },
+    designation: { type: String, enum: [ 'store_manager', 'assistant_manager', 'cashier', 'sales_associate', 'inventory_clerk', 'security', 'janitor', 'other' ], required: false },
          status: { type: String, enum: ['active', 'terminated', 'on_leave', 'resigned'], default: 'active' },
      permission: { type: [String], enum: ['r:delivery-partner', 'w:delivery-partner', 'r:shop', 'w:shop', 'r:product', 'c:product', 'w:shop', 'r:category', 'c:category', 'w:category']},
       startDate: { type: Date, required: false,},
