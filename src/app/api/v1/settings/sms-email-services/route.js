3388c6c47fdddba06f365f02e8ef48c74524816f
rimaakter1 <rimac201262@gmail.com>
2025-08-09 17:26:12 +0600

    ...

--- src/app/api/v1/settings/sms-email-services/route.js
+++ src/app/api/v1/settings/sms-email-services/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import authDbConnect from "@/lib/mongodb/authDbConnect";
 import vendorDbConnect from "@/lib/mongodb/vendorDbConnect";
-import getAuthenticatedUser from "../auth/utils/getAuthenticatedUser";
+import getAuthenticatedUser from "../../auth/utils/getAuthenticatedUser";
 import { shopModel } from "@/models/auth/Shop";
 import { vendorModel } from "@/models/vendor/Vendor";
 import { applyRateLimit } from "@/lib/rateLimit/rateLimiter";
