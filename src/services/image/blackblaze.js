f9ea4efc505b67abd657f2380557e840c82d2cbc
MamunApidoxy <mamun@apidoxy.com>
2025-08-11 18:41:56 +0600

    ....

--- src/services/image/blackblaze.js
+++ src/services/image/blackblaze.js
@@ -242,7 +242,7 @@ export async function downloadImage({bucket, folder, file}) {
     await authorizeB2();
     return await b2.downloadFileByName({
                       bucketName: bucket,
-                      fileName: `${file}`,
+                      fileName: `${folder}/${file}`,
                       responseType: 'stream',
                     });
   } catch (error) {
