87b231a1cc83b7c421377723acc289b20e46b75e
rimaakter1 <rimac201262@gmail.com>
2025-08-23 18:23:32 +0600

    create post route for cart

--- src/app/api/v1/cart/item/cartDTOSchema.js
+++ src/app/api/v1/cart/item/cartDTOSchema.js
@@ -4,9 +4,7 @@ export const cartDTOSchema = z.object({
     productId: z.string().min(1, "Product ID is required"),
     variantId: z.string().optional(),
      quantity: z.number().int().min(1, "Quantity must be at least 1").optional(),
-       action: z.enum(['inc', 'dec', '+', '-']),
-//   fingerprint: z.string().optional(),
-       coupon: z.string().optional(),        // can be inferred from user session too
+       coupon: z.string().optional(),        
 });
 
 export default cartDTOSchema
