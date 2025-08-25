ebeefebe7d560ec4dd88511463887ad6ccbe0e88
rimaakter1 <rimac201262@gmail.com>
2025-08-25 13:57:06 +0600

    ...

--- src/app/api/v1/user/register/route.js
+++ src/app/api/v1/user/register/route.js
@@ -12,29 +12,33 @@ import config from "../../../../../../config";
 
 export async function POST(request) {
   let body;
-  try { body = await request.json(); } 
-  catch { return NextResponse.json({ error: "Invalid JSON" }, { status: 400 });}
-  
-  const      parsed = registerDTOSchema.safeParse(body);
+  try {
+    body = await request.json();
+  }
+  catch { return NextResponse.json({ error: "Invalid JSON" }, { status: 400 }); }
+
+  const parsed = registerDTOSchema.safeParse(body);
+  console.log(parsed)
+
   const referenceId = request.headers.get('x-vendor-identifier');
-  const        host = request.headers.get('host'); 
+  const host = request.headers.get('host');
 
   const ip = request.headers.get('x-forwarded-for')?.split(',')[0]?.trim() || request.headers.get('x-real-ip') || request.socket?.remoteAddress || '';
   // const fingerprint = request.headers.get('x-fingerprint') || null;
 
-  const { allowed, retryAfter } = await applyRateLimit({ key: `${host}${ip}`, scope: 'userLogin'  });
+  const { allowed, retryAfter } = await applyRateLimit({ key: `${host}${ip}`, scope: 'userLogin' });
   if (!allowed) return NextResponse.json({ error: 'Too many requests. Please try again later.' }, { status: 429, headers: { 'Retry-After': retryAfter.toString() } });
 
-  if (!parsed.success)    return NextResponse.json({ error: "Invalid input" }, { status: 422 });
+  if (!parsed.success) return NextResponse.json({ error: "Invalid input" }, { status: 422 });
   if (!referenceId && !host) return NextResponse.json({ error: "Missing vendor identifier or host" }, { status: 400 });
 
-  const     name = parsed.data.name?.trim()                 || null;
-  const    email = parsed.data.email?.trim().toLowerCase()  || null;
-  const    phone = parsed.data.phone?.trim()                || null;
-  const password = parsed.data?.password                    || null;
-  const   gender = parsed.data?.gender                      || null;
+  const name = parsed.data.name?.trim() || null;
+  const email = parsed.data.email?.trim().toLowerCase() || null;
+  const phone = parsed.data.phone?.trim() || null;
+  const password = parsed.data?.password || null;
+  const gender = parsed.data?.gender || null;
 
-  if(password){
+  if (password) {
     // add more containing common password
     const commonPasswords = ['password', '12345678', 'qwerty123'];
     if (commonPasswords.includes(password.toLowerCase())) return NextResponse.json({ error: "Common password provided, change the password" }, { status: 422 });
@@ -42,54 +46,56 @@ export async function POST(request) {
 
   try {
     const { data, dbUri, dbName } = await getInfrastructure({ referenceId, host })
-    if(!data) return NextResponse.json({ error: "Invalid request" }, { status: 400 });
+    if (!data) return NextResponse.json({ error: "Invalid request" }, { status: 400 });
 
 
-    const      shop_db = await dbConnect({ dbKey: dbName, dbUri })
-    const         User = userModel(shop_db)
-    const existingUser = await User.findOne({ $or: [ ...(email ? [{ email }] : []), ...(phone ? [{ phone }] : []) ]});
+    const shop_db = await dbConnect({ dbKey: dbName, dbUri })
+    const User = userModel(shop_db)
+    const existingUser = await User.findOne({ $or: [...(email ? [{ email }] : []), ...(phone ? [{ phone }] : [])] });
     if (existingUser) return NextResponse.json({ error: "User already exists" }, { status: 409 });
-    
-    const               SALT_ROUNDS =  parseInt( process.env.END_USER_BCRYPT_SALT_ROUNDS || "10", 10);
-    const                      salt = await bcrypt.genSalt(SALT_ROUNDS);
-    
-    const  rawToken = crypto.randomBytes(48).toString('hex')
-    const     token = crypto.createHash('sha256').update(rawToken).digest('hex');
-    const       otp = crypto.randomInt(Number(process.env.PHONE_OTP_MIN), Number(process.env.PHONE_OTP_MAX)).toString();
-    const   otpSalt = await bcrypt.genSalt(12);
+
+    const SALT_ROUNDS = parseInt(process.env.END_USER_BCRYPT_SALT_ROUNDS || "10", 10);
+    const salt = await bcrypt.genSalt(SALT_ROUNDS);
+
+    const rawToken = crypto.randomBytes(48).toString('hex')
+    const token = crypto.createHash('sha256').update(rawToken).digest('hex');
+    const otp = crypto.randomInt(Number(process.env.PHONE_OTP_MIN), Number(process.env.PHONE_OTP_MAX)).toString();
+    const otpSalt = await bcrypt.genSalt(12);
     const hashedOtp = await bcrypt.hash(otp, otpSalt)
 
     const emailVerificationExpireMinutes = Number.isFinite(Number(data.expirations?.emailVerificationExpireMinutes ?? config.emailVerificationDefaultExpireMinutes))
-                                                         ? Number(data.expirations?.emailVerificationExpireMinutes ?? config.emailVerificationDefaultExpireMinutes)
-                                                         : 15;
+      ? Number(data.expirations?.emailVerificationExpireMinutes ?? config.emailVerificationDefaultExpireMinutes)
+      : 15;
 
     const phoneVerificationExpireMinutes = Number.isFinite(Number(data.expirations?.phoneVerificationExpireMinutes ?? config.phoneVerificationDefaultExpireMinutes))
-                                                         ? Number(data.expirations?.phoneVerificationExpireMinutes ?? config.phoneVerificationDefaultExpireMinutes)
-                                                         : 4;
+      ? Number(data.expirations?.phoneVerificationExpireMinutes ?? config.phoneVerificationDefaultExpireMinutes)
+      : 4;
 
-    const payload = {         name,
-                          security: { ...(password && { password: await bcrypt.hash(password, salt) } ) },
-                      verification: { ...(email && {
-                                                emailVerificationToken: token,
-                                          emailVerificationTokenExpiry: new Date(Date.now() + (emailVerificationExpireMinutes * 60 * 1000) ).getTime(),                                    
-                                        }),
-                                      ...((phone && !email) && { 
-                                                  phoneVerificationOTP: hashedOtp,
-                                            phoneVerificationOTPExpiry: new Date(Date.now() + (phoneVerificationExpireMinutes * 60 * 1000)).getTime()
-                                        })
-                                    },
-                    ...(email  && {  email: email.trim().toLowerCase() }),
-                    ...(phone  && {  phone: phone?.trim() }),
-                    ...(gender && { gender: gender?.trim() })
-                  };
+    const payload = {
+      name,
+      security: { ...(password && { password: await bcrypt.hash(password, salt) }) },
+      verification: {
+        ...(email && {
+          emailVerificationToken: token,
+          emailVerificationTokenExpiry: new Date(Date.now() + (emailVerificationExpireMinutes * 60 * 1000)).getTime(),
+        }),
+        ...((phone && !email) && {
+          phoneVerificationOTP: hashedOtp,
+          phoneVerificationOTPExpiry: new Date(Date.now() + (phoneVerificationExpireMinutes * 60 * 1000)).getTime()
+        })
+      },
+      ...(email && { email: email.trim().toLowerCase() }),
+      ...(phone && { phone: phone?.trim() }),
+      ...(gender && { gender: gender?.trim() })
+    };
 
-        const newEndUser = new User(payload);
-        const savedUser =  await newEndUser.save();
-    
-console.log(payload)
+    const newEndUser = new User(payload);
+    const savedUser = await newEndUser.save();
 
-    if(savedUser){
-      if(email && password) {
+    console.log(payload)
+
+    if (savedUser) {
+      if (email && password) {
         const senderEmail = data.email || process.env.DEFAULT_SENDER_EMAIL;
         /** 
          ** ****************************************************** **
@@ -99,18 +105,18 @@ console.log(payload)
          ** sendEmail()                                            **
          ** sendSMS()                                              **
          ** ****************************************************** **
-         **/ 
-        sendEmail({ receiverEmail: savedUser.email, emailType: 'VERIFY' , senderEmail, token: token  }).catch(console.error);
+         **/
+        sendEmail({ receiverEmail: savedUser.email, emailType: 'VERIFY', senderEmail, token: token }).catch(console.error);
       }
-      if(phone && !email) await sendSMS({ phone, message: `Your verification code is: ${otp}`}).catch(console.error);
+      if (phone && !email) await sendSMS({ phone, message: `Your verification code is: ${otp}` }).catch(console.error);
       return NextResponse.json({ success: true, message: "User registered successfully" }, { status: 201 });
-    } 
+    }
 
   } catch (error) {
     console.log(`User Creation Error: ${error.message}`);
-    return NextResponse.json( { error: "Internal server error" }, { status: 500 });
+    return NextResponse.json({ error: "Internal server error" }, { status: 500 });
   }
 }
 
 
-    // const { vendor, dbUri, dbName } = await getVendor({ id: vendorId, host, fields: ['createdAt', 'primaryDomain']    });
+// const { vendor, dbUri, dbName } = await getVendor({ id: vendorId, host, fields: ['createdAt', 'primaryDomain']    });
